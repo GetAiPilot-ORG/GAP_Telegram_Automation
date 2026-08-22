a1f85ab9b05edef7c72430612a68ff7ae711db2b
Unreala9 <shwetchourey3@gmail.com>
2026-08-22 15:33:12 +0530

    c

--- GetaipilotBackEnd1/Telesub.py
+++ GetaipilotBackEnd1/Telesub.py
@@ -603,5 +603,5 @@ if __name__ == "__main__":
     import uvicorn
 
     host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", "8000"))
+    port = int(os.getenv("PORT", "8002"))
     uvicorn.run(app, host=host, port=port)
